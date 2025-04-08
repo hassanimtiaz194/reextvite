f72d7f80408dfd872ecb89972bdcd1b498459090
hassanimtiaz194 <hassan.imtiaz194@gmail.com>
2025-04-08 21:04:12 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reext from './node_modules/@sencha/reext/dist/ReExt/vite-plugin-reext.js'
+import reext from './plugins/vite-plugin-reext';
 
 export default defineConfig({
   plugins: [
     react(), 
     reext()
-  ]
+  ],
+  build: {
+    outDir: 'dist',
+    target: 'esnext',  // Or set based on your compatibility needs
+  }
 })
