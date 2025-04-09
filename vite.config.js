b89d1c007f7c5f26538b5072d78bb38b80ed5b10
hassanimtiaz194 <hassan.imtiaz194@gmail.com>
2025-04-09 20:04:00 +0500

    Revert "changes"
    This reverts commit f72d7f80408dfd872ecb89972bdcd1b498459090.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reext from './plugins/vite-plugin-reext';
+import reext from './node_modules/@sencha/reext/dist/ReExt/vite-plugin-reext.js'
 
 export default defineConfig({
   plugins: [
     react(), 
     reext()
-  ],
-  build: {
-    outDir: 'dist',
-    target: 'esnext',  // Or set based on your compatibility needs
-  }
+  ]
 })
